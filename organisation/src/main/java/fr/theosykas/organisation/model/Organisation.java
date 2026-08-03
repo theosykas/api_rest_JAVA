17b5798c9e936376ed9b4069383d0558653ef5fc
theosykas <theosykas@icloud.com>
2026-08-03 21:48:17 +0200

    feat: package/import

--- organisation/src/main/java/fr/theosykas/organisation/model/Organisation.java
+++ organisation/src/main/java/fr/theosykas/organisation/model/Organisation.java
@@ -1,5 +1,18 @@
+package fr.theosykas.organisation.model;
+import jakarta.persistence.*;
+// Java Persistence API (JPA)
+
+// hibernate
 
 
+// entity == table pour JPA
+@Entity
+@Table(name = "Organisation")  // __tablename__
 public class Organisation {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // + 1 -> line
+	private Long id_pos_db;
 
+	@Column(name = "name", length = 200) // modifier les lignes
+	private String name;
 }
