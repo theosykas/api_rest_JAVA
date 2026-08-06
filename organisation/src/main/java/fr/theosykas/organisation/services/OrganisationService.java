cfd4eb5b580f15155fb988ad4a419233e2765b75
theosykas <theosykas@icloud.com>
2026-08-06 09:57:42 +0200

    Org

--- organisation/src/main/java/fr/theosykas/organisation/services/OrganisationService.java
+++ organisation/src/main/java/fr/theosykas/organisation/services/OrganisationService.java
@@ -2,10 +2,10 @@ package fr.theosykas.organisation.services;
 import fr.theosykas.organisation.model.Organisation;
 import fr.theosykas.organisation.repository.OrganisationRepository;
 import jakarta.transaction.Transactional;
-
+import java.lang.reflect.Member;
 import org.springframework.stereotype.Service;
-@Service
 
+@Service
 public class OrganisationService {
 	private final OrganisationRepository repository;
 
@@ -14,9 +14,28 @@ public class OrganisationService {
 	}
 
 	@Transactional
-	public Organisation GetOrgById(Long orgId, String newName) {
-		Organisation update_org = OrganisationRepository.findById(orgId)
-			.orElseThrow(() -> new OrganisationNotFoundException(id));
+	public Organisation CreateOrganisation(String orgName) {
+		Organisation create_organisation = repository.save(orgName);
+		Member member = new Member();
+		return create_organisation;
 	}
 
+	@Transactional // Make 1 commit or rollback
+	public Organisation GetOrgById(Long orgId) {
+		Organisation org_id = repository.findById(orgId)
+			.orElseThrow(() -> new OrganisationNotFoundException(orgId));
+		return org_id;
+	}
+
+	@Transactional
+	public Organisation UpdateOranisation(Long orgId, String orgName) {
+		Organisation update_org = GetOrgById(orgId);
+		update_org.setName(orgName);
+		return update_org;
+	}
+
+	@Transactional // return void
+	public void DeleteOrganisation(Long orgId, String orgName) {
+		repository.delete(GetOrgById(orgId));
+	}
 }
